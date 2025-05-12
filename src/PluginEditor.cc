4ebcf8f63cb79a9f56ca141dc3e3fb01008be77a
MK <matthew.krecker@outlook.com>
2025-05-12 14:12:40 -0500

    more

---
+++ src/PluginEditor.cc
@@ -0,0 +1,26 @@
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
+    : AudioProcessorEditor (&p), processorRef (p)
+{
+    juce::ignoreUnused (processorRef);
+    setSize (200, 200);
+}
+
+AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
+{
+}
+
+void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("OJ", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void AudioPluginAudioProcessorEditor::resized()
+{
+}
---
+++ src/PluginEditor.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "PluginProcessor.h"
+
+//==============================================================================
+class AudioPluginAudioProcessorEditor final : public juce::AudioProcessorEditor
+{
+public:
+    explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
+    ~AudioPluginAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    AudioPluginAudioProcessor& processorRef;
+    juce::Slider gain;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
+};
